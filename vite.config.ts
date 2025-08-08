3b797feb436a1b2c9266c9e3b6a8dd7b51eb0ec9
solidsnail <rachiduoad@gmail.com>
2025-08-08 11:21:03 +0100

    fix(docs): fix asset import

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,14 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "https://solidsnail.github.io/llmini.js/",
+  optimizeDeps: {
+    include: ["kokoro-js"],
+  },
   build: {
     outDir: "docs",
+    rollupOptions: {
+      external: [],
+    },
   },
   worker: {
     format: "es",
